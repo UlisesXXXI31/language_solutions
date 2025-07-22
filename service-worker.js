4674df3366212e6cf77fdbff29b29d91c4909438
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2025-07-22 13:33:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,64 @@
-const CACHE_NAME = "deutschapp-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/style.css",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+// This is the "Offline page" service worker
 
-// Instalación
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); // Activación inmediata
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "deutschapp-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Activación
-self.addEventListener("activate", event => {
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
+    caches.open(CACHE)
+      .then((cache) => cache.addAll([
+        offlineFallbackPage,
+        '/',
+        '/index.html',
+        '/app.js',
+        '/style.css',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        '/palabras.json',
+        '/service-worker.js',
+        '/offline.html',
+        '/EIS.jpg',
+        '/correcto.mp3',
+        '/incorrecto.mp3'
+
+        // Añade aquí todos los recursos que necesites cachear
+      ]))
   );
-  self.clients.claim(); // Toma el control de las páginas inmediatamente
 });
 
-// Intercepción de peticiones
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      // Devuelve desde caché si existe, o hace fetch normalmente
-      return response || fetch(event.request);
-    })
-  );
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
